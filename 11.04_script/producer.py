26cea5db0d3d66fd6cf983dedb8e125cc6b70641
Mikhail-2023 <123817729+Mikhail-2023@users.noreply.github.com>
2023-07-23 20:40:32 +0300

    Update producer.py

--- 11.04_script/producer.py
+++ 11.04_script/producer.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.176'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.151'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
