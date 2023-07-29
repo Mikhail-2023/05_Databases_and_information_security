4e003179d3460c15480b261bafca2bff30d6e58a
Mikhail-2023 <123817729+Mikhail-2023@users.noreply.github.com>
2023-07-29 18:43:31 +0300

    Delete producer.py

--- 11.04_script/producer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.151'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
-connection.close()
